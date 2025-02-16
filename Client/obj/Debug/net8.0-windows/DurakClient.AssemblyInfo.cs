02e56dd2f9b038a80ce1d08abd10571c03644e52
Momchil Kalestrov <kalestrov.mo@gmail.com>
2025-02-16 18:50:30 +0200

    add a proper gitignore

--- Client/obj/Debug/net8.0-windows/DurakClient.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0-windows/DurakClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DurakClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0fe2490d807ffa9c4a82e7517c99c889729011")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a180a86a5cda5642420a7d2456778d7bcbce027f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DurakClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DurakClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DurakData/obj/Debug/net8.0/DurakData.AssemblyInfo.cs
+++ DurakData/obj/Debug/net8.0/DurakData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DurakData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0fe2490d807ffa9c4a82e7517c99c889729011")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a180a86a5cda5642420a7d2456778d7bcbce027f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DurakData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DurakData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
